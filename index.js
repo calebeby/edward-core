ba0ebf8544b366b7fd738e5532a6993ebac5121f
Caleb Eby <caleb.eby01@gmail.com>
2017-06-20 11:02:29 -0700

    Say more stuff!

--- index.js
+++ index.js
@@ -3,3 +3,5 @@ const {spawn} = require('child_process')
 const say = text => new Promise(resolve => spawn('flite', ['-t', `"${text}"`]).on('exit', resolve))
 
 say('hello. I am Edward')
+  .then(() => say('I have finished saying the previous clause'))
+  .then(() => say('How are you today'))
