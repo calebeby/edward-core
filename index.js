9d653121c1a790e1dede4fa04ca7fcf4daa2f1aa
Caleb Eby <caleb.eby01@gmail.com>
2017-06-20 11:00:02 -0700

    Add flite

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const {spawn} = require('child_process')
 
-spawn('speaker-test')
+const say = text => new Promise(resolve => spawn(`flite -t ${text}`).on('exit', resolve))
 
+say('hello. I am Edward')
